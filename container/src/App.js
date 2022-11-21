14f67c62aceb6dac9153ec46116786d1552bb2a0
Patrick Darbee <pdarbee@installationmadeeasy.com>
2022-11-21 16:45:51 -0500

    Initial Commit

---
+++ container/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+export default function App() {
+  return (
+    <>
+      <h1>HELLO</h1>
+      <h1>This is mounted</h1>
+      <MarketingApp />
+    </>
+  );
+}
