e1ae5efa1cc45a855edc43819a8a4b8c23327d5c
Patrick Darbee <pdarbee@installationmadeeasy.com>
2022-11-22 21:14:49 -0500

    adding container webapp yaml

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -10,7 +10,7 @@ const mount = (el) => {
 // If we are in development and in isolation,
 // call mount immediately
 if (process.env.NODE_ENV === "development") {
-  const devRoot = document.querySelector("#root");
+  const devRoot = document.querySelector("#_marketing-dev-root");
 
   if (devRoot) {
     mount(devRoot);
